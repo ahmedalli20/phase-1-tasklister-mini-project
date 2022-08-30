cf741a968544194e1d0a92939873deff6678803f
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-08-30 11:12:34 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -37,26 +37,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-  // })let form = document.querySelector('form')
+let form = document.querySelector('form')
 
-  // form.addEventListener('submit', (e) => {
-  //   e.preventDefault()
-  //   buuildToDo(e.target.new_task_description.value)
-  //   form.reset()
-
-  // function buildToDo(todo){
-  //   let p = document.createElement('p')
-  //   let btn = d0cument.createElement('button')
-  //   btn.addEventListener('click',handleDelete)
-  //   btn.textContent = 'Cancel'
-  //   p.textContent = `${todo} `
-  //   p.appendChild(btn)
-  //   document.querySelector('#list').appendChild(p)
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buuildToDo(e.target.new_task_description.value)
+    form.reset()
+  })
+  function buildToDo(todo){
+    let p = document.createElement('p')
+    let btn = d0cument.createElement('button')
+    btn.addEventListener('click',handleDelete)
+    btn.textContent = 'Cancel'
+    p.textContent = `${todo} `
+    p.appendChild(btn)
+    document.querySelector('#list').appendChild(p)
  
-  // }
-  //   function handleDelete(e){
-  //    e.target.parentNode.remove()
-  //   }
+  }
+    function handleDelete(e){
+     e.target.parentNode.remove()
+    }
 
  
-
+  
